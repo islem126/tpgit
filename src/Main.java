035b9367d46cfd82470dfa9c6ca644347b6e8d87
islem <117855994+islem201@users.noreply.github.com>
2025-10-17 16:34:02 +0200

    Ajout de commentaires dans Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Affiche un message de bienvenue
+        System.out.println("Hello, world!");
+        
+        // Vous pouvez ajouter d'autres fonctionnalités ici
+    }
+}
